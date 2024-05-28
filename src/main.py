e08a69aeb797060baff2e95399afe819033a05d1
Matheus Chaves <matheusfonsecachaves@gmail.com>
2024-05-28 01:37:40 -0300

    refactor: refactored main API config

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, HTTPException, Depends
 from src.router.api import router
+from src.config.docs import api_description, tags_metadata
 from src.scraper import scrape_all_pages
 from src.auth import get_current_user
 import threading
@@ -26,75 +27,10 @@ async def lifespan(app: FastAPI):
     threading.Thread(target=scrape_all_pages_with_lock).start()
     yield
 
-
-tags_metadata = [
-    {
-        "name": "Autenticação",
-        "description": "- Operações com usuários e sua autenticação.",
-    },
-    {
-        "name": "Ajuda",
-        "description": "- Rotas de apoio para utilizar a aplicação. Não são "
-        "obrigatórias para o funcionamento da API.",
-        "externalDocs": {
-            "description": "Items external docs",
-            "url": "https://fastapi.tiangolo.com/",
-        },
-    },
-    {
-        "name": "Produção",
-        "description": "- Produção de vinhos, sucos e derivados do Rio Grande do Sul.",
-        "externalDocs": {
-            "description": "Site da Embrapa",
-            "url": "http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_02",
-        },
-    },
-    {
-        "name": "Processamento",
-        "description": "- Quantidade de uvas processadas no Rio Grande do Sul.",
-        "externalDocs": {
-            "description": "Site da Embrapa",
-            "url": "http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_03",
-        },
-    },
-    {
-        "name": "Comercialização",
-        "description": "- Comercialização de vinhos e derivados no Rio Grande do Sul.",
-        "externalDocs": {
-            "description": "Site da Embrapa",
-            "url": "http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_04",
-        },
-    },
-    {
-        "name": "Importação",
-        "description": "- Importação de derivados de uva.",
-        "externalDocs": {
-            "description": "Site da Embrapa",
-            "url": "http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_05",
-        },
-    },
-    {
-        "name": "Exportação",
-        "description": "- Exportação de derivados de uva.",
-        "externalDocs": {
-            "description": "Site da Embrapa",
-            "url": "http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_06",
-        },
-    },
-]
-
 app = FastAPI(
     debug=True,
     title="Tech Challenge - Dados da Vitivinicultura da Embrapa",
-    description=(
-        "Esta API fornece dados sobre a vitivinicultura coletados pela Embrapa.\n\n"
-        "Através das rotas abaixo, pode-se acessar informações referentes à quantidade de uvas processadas, produção e comercialização de vinhos, suco e derivados provenientes do Estado do Rio Grande do Sul, além dos dados de importações e exportações dos produtos da vitivinicultura.\n\n"
-        "Para mais informações, acesse o [README.md do projeto](https://github.com/matheus-chaves/api-consulta-dados-embrapa/blob/develop/README.md).\n\n"
-        "### Outras dúvidas? Contate os membros do grupo 39:\n"
-        "- [Bruno Pinheiro Mendes](mailto:brupmendes@icloud.com)\n"
-        "- [Matheus Fonseca Chaves](mailto:matheusfonsecachaves@gmail.com)\n"
-        "- [Michelle Da Luz Rodrigues](mailto:michellerodrigues268@gmail.com)\n"
-    ),
+    description=api_description,
     version="1.0.0",
     license_info={
         "name": "Licença MIT",
